3ceb2f3482450d967c4d79f1448ffb6dea035e98
Ravindu Suriarachchi <Ravindu Suriarachchi>
2016-12-14 00:55:34 +0530

    Impliment Admin Autentication

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,9 +10,17 @@
 angular.module('yapp')
   .controller('LoginCtrl', function($scope, $location) {
 
-    $scope.submit = function() {
+  	$scope.erro = false;
 
-      $location.path('/dashboard');
+    $scope.submit = function(auth) {
+    	console.log(auth);
+
+    	if(auth.email == "admin" && auth.password == "admin"){
+			$location.path('/dashboard');
+			$scope.erro = false;    	}
+		else{
+			$scope.erro = true;
+		}
 
       return false;
     }
